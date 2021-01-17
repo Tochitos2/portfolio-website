e67e010b432583055fc64c5aec41b0f05b21ee0e
Tochi <Tochitos2@gmail.com>
2021-01-17 09:13:13 +0000

    Functional implementation of projects section and sub-components.

--- src/components/Projects/ProjectsElements.js
+++ src/components/Projects/ProjectsElements.js
@@ -2,42 +2,61 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 class Project extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
-            <ProjectContainer>
-            <Title> { this.props.title } </Title>
-            <Description> { this.props.desc } </Description>
-            <Icon src= {`/icons/${this.props.imageName}.jpg`} alt= {this.props.altText} />
-            </ProjectContainer>
+            <ProjectCard className= { this.props.className } onClick={ () => window.open( this.props.href ) }>
+            <Title float={ this.props.float }> { this.props.title } </Title>
+            <Description float={ this.props.float }> { this.props.description } </Description>
+            <Icon float={ this.props.float } src={`/icons/${this.props.iconPath}.jpg`} alt={this.props.altText} />
+            </ProjectCard>
         );        
     }
+
 }
 
 export default Project;
 
-const ProjectContainer = styled.div`
-
+const ProjectCard = styled.div`
+    flex: 0 0 min(30rem, 100%);
+    margin: 0 1rem 2rem 1rem;
+    border: 1px solid ${ props => props.theme.borderColour };
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
 `;
 
 const Title = styled.h3`
-
+    font-family: ${ props => props.theme.fontFamily };
+    grid-row: 1 / 2;
+    grid-column: ${ props => props.float === "left" ? "1 / 2" : "2 / 3" };
+    text-align: ${ props => props.float };
 `;
 
 const Description = styled.p`
-
+    font-family: ${ props => props.theme.fontFamily };
+    grid-row: 2 / 3;
+    grid-column: ${ props => props.float === "left" ? "1 / 2" : "2 / 3" };
+    text-align: ${ props => props.float };
 `;
 
 const Icon = styled.img`
-
+    grid-row: 1 / 3;
+    grid-column: ${ props => props.float === "left" ? "2 / 3" : "1 / 2" };
 `;
 
-export const SectionContainer = styled.section`
-    margin: 10%;
+export const SectionTitle = styled.h2`
+    text-align: center;
+    font-family: ${ props => props.theme.fontFamily };
+`;
+
+export const ProjectsContainer = styled.section`
+    margin: 2rem 10vw;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+
+    @media (max-width: 500px) {
+        margin: 2rem 5vw;
+    }
 `;
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,23 +1,92 @@
 import React from 'react';
-import {Project, SectionContainer} from './ProjectsElements';
+import Project, {SectionTitle, ProjectsContainer} from './ProjectsElements';
 
 class Projects extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = { 
+            // Array holding the details of each project to be displayed, used for automatic JSX generation.
+            projects: [
+                { title: "First", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+                { title: "Second", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+                { title: "Third", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+                { title: "Fourth", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+                { title: "fifth", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+                { title: "sixth", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+                { title: "seventh", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+                { title: "eighth", description: "test", href: "", iconPath: "", altText: "", float: "left" },
+            ]
+        };    
     }
 
     render() {
         return (
-            <SectionContainer>
-                <Project />
-                <Project />
-                <Project />
-                <Project />
-                <Project />
-                <Project />
-            </SectionContainer>
+            <div>
+                <SectionTitle>Projects</SectionTitle>
+
+                <ProjectsContainer>
+                    {this.state.projects.map((project) => {
+                        return (<Project className=    "project-card" 
+                                         key=          {project.title}
+                                         title=        {project.title} 
+                                         description=  {project.description} 
+                                         href=         {project.href} 
+                                         iconPath=     {project.iconPath}
+                                         altText=      {project.altText}
+                                         float=        {project.float} />);
+                    })}
+                </ProjectsContainer>               
+            </ div>
         );
     }
+
+    componentDidMount() {
+        this.setAlignment();
+
+        // Recalculate and update card alignment when window size changes.
+        window.addEventListener('resize', this.setAlignment.bind(this));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.setAlignment);
+    }
+
+    /**
+     * Updates state to correct project card alignment.
+     */
+    setAlignment() {
+        // Get the number of columns at current render (on a full row).
+        let columnCount = 0;
+        let currentTop = 0, prevTop = 0;
+        const items = document.getElementsByClassName("project-card");
+        for(let i=0; i < items.length; i++) {
+            currentTop = items[i].getBoundingClientRect().top;
+            if(i !==0 && currentTop !== prevTop) break;
+            columnCount++;
+            prevTop = currentTop;
+        }
+
+        // Create a copy of the project array from state with updated floats
+        let newProjects = this.state.projects;
+        // Get the width of the final row, as this may be smaller than the rest.
+        let finalRowWidth = this.state.projects.length % columnCount;
+        
+        for(let i = 0; i < this.state.projects.length; i++) {
+            // Find the x value in the grid, i.e. the column number at current rendering.
+            let column = (i + 1) % columnCount;
+            // Is the card on the final row?
+            let isOnFinalRow = i + 1 > this.state.projects.length - finalRowWidth;
+            // If card is on final row, use that row width instead.
+            let cardsOnRow = isOnFinalRow ? finalRowWidth : columnCount;
+
+            // Pass the appropriate float prop for that card's position.
+            newProjects[i].float = ((column !== 0 || columnCount === 1) && column <= Math.ceil(cardsOnRow / 2) ) ? "left" : "right";
+        }
+        // Update state to hold revised projects array.
+        this.setState({ projects: newProjects });
+    }
+
 }
 
 export default Projects;
