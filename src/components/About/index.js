c1ff446d4eea6380e84a1d8275059f8585421df9
Tochi <Tochitos2@gmail.com>
2021-01-23 17:54:54 +0000

    Added about section.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,44 @@
+import React from 'react';
 import styled, { css } from 'styled-components';
+import { SectionTitle } from '../Container';
 
-const About = styled.div``;
+export default class About extends React.Component{
 
-export default About;
+    render() {
+        return(
+            <>
+                <SectionTitle>About</SectionTitle>
+
+                <AboutContainer>
+                <StandOut>Hi! </StandOut>
+                <Paragraph>{ text }</Paragraph>
+                </AboutContainer>
+            </>
+        );
+    }
+}
+const AboutContainer = styled.div`
+    margin: 2rem 20vw;
+
+    @media (max-width: 650px) {
+        margin: 2rem 10vw;
+    }
+`;
+
+const Paragraph = styled.p`
+    font-family: ${props => props.theme.fontFamily};
+    font-weight: ${props => props.theme.fontWeight};
+    font-size: 1.5rem;
+    color: ${props => props.theme.textColour};
+    display: inline;
+`;
+
+const StandOut = styled(Paragraph)`
+    display: inline;
+    font-size: 4rem;
+    font-weight: normal;
+    font-style: italic;
+    padding-right: 1.5rem;
+`;
+
+const text = "I'm a second year Computer Science student with an interest in Cyber Security, Web Design, Reverse Engineering, Game Modding, and using Computer Science to advance our climate response and create a positive impact.";
